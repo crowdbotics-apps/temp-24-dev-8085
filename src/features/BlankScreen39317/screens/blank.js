6fe257e30e7dccc54f7f7ef8c1f4ac1f6f5bd713
Crowdbotics <team@crowdbotics.com>
2020-07-24 06:49:49 +0000

    Update components

--- src/features/BlankScreen39317/screens/blank.js
+++ src/features/BlankScreen39317/screens/blank.js
@@ -303,6 +303,42 @@ export class _Blank extends React.Component {
       >
         Press me!
       </Button>
+      <Button
+        style={{
+          width: "100%",
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#33ff41",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textShadow: "0pt 0pt 0pt #000000",
+          textDecoration: "none",
+          textTransform: "none",
+          lineHeight: 1
+        }}
+        onPress={() => alert("Pressed!")}
+      >
+        Press me!
+      </Button>
     </ImageBackground>
   )
 }

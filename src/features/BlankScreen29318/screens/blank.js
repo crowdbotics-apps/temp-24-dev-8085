904d5a89918e976e6b9e2b6743318f870f30b5a2
Crowdbotics <team@crowdbotics.com>
2020-07-24 07:41:22 +0000

    Update components

--- src/features/BlankScreen29318/screens/blank.js
+++ src/features/BlankScreen29318/screens/blank.js
@@ -22,7 +22,12 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = { CheckBox_11: true, Radio_12: true, Toggle_13: true }
+  state = {
+    CheckBox_11: true,
+    Radio_12: true,
+    Toggle_13: true,
+    Toggle_54: true
+  }
 
   render = () => (
     <View
@@ -96,6 +101,40 @@ export class _Blank extends React.Component {
       >
         Press me!
       </Button>
+      <Text
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textShadow: "0pt 0pt 0pt #000000",
+          textDecoration: "none",
+          textTransform: "none",
+          lineHeight: 1
+        }}
+      >
+        Sample text content
+      </Text>
       <CheckBox
         text="Checkbox"
         disabled={false}
@@ -127,6 +166,40 @@ export class _Blank extends React.Component {
         checked={this.state.CheckBox_11}
         onChange={nextChecked => this.setState({ CheckBox_11: nextChecked })}
       />
+      <Text
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textShadow: "0pt 0pt 0pt #000000",
+          textDecoration: "none",
+          textTransform: "none",
+          lineHeight: 1
+        }}
+      >
+        Sample text content
+      </Text>
       <Radio
         text="Radio button"
         disabled={false}
@@ -218,6 +291,39 @@ export class _Blank extends React.Component {
           borderBottomWidth: 0,
           borderRadius: 0
         }}
+      />
+      <Toggle
+        text="switch ON/OFF"
+        activeColor="#409EFF"
+        inactiveColor="#C0CCDA"
+        disabled={false}
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textShadow: "0pt 0pt 0pt #000000",
+          textDecoration: "none",
+          textTransform: "none",
+          lineHeight: 1
+        }}
+        checked={this.state.Toggle_54}
+        onChange={nextChecked => this.setState({ Toggle_54: nextChecked })}
       />
       <Image
         resizeMode="stretch"
